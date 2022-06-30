bf66b80207044d14897d2140b40b0d7888614270
bennk0509 <nkakiet21@clc.fitus.edu.vn>
2022-06-30 15:16:51 +0700

    Update server.py

--- server.py
+++ server.py
@@ -7,54 +7,37 @@ import hotel
 import user
 import bill
 
-HOST= "127.0.0.1"
-SERVER_PORT = 65432
-FORMAT = "utf8"
-
-def recvList(conn):
-    list = []
-
-    item = conn.recv(1024).decode(FORMAT)
-
-    while (item != "end"):
-        
-        list.append(item)
-        #response
-        conn.sendall(item.encode(FORMAT))
-        item = conn.recv(1024).decode(FORMAT)
-    
-    return list
-
 def handleClient(conn: socket, addr):
     
     print("conn:",conn.getsockname())
-    
     msg = None
     while (msg != "x"):
         msg = conn.recv(1024).decode(FORMAT)
         print("client ",addr, "says", msg)
-        
-    print("client" , addr, "finished")
+    print("client" , addr, "has left the sever")
     print(conn.getsockname(), "closed")
     conn.close()
+              
+clients = {}
+addresses= {}
+HOST= "127.0.0.1"
+SERVER_PORT = 65432
+BUFSIZE = 1024
+FORMAT = "utf8"
 
 def main():
 
     # You can wirte the functions for socket here
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    
     s.bind((HOST, SERVER_PORT))
     s.listen()
-
     print("SERVER SIDE")
     print("server:", HOST, SERVER_PORT)
     print("Waiting for Client")
-
     nClient = 0
     while (nClient < 3):
         try:
             conn, addr = s.accept()
-            
             thr = threading.Thread(target=handleClient, args=(conn,addr))
             thr.daemon = False
             thr.start()
@@ -63,11 +46,8 @@ def main():
             print("Error")
         
         nClient += 1
-
     print("End")
-
     s.close();
-    conn.close()
     # Here is used to test functions in link_data.py
     # Load hotel data from file
     root_path = "Data/"
