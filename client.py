1a96bccc71d8520d825dcf00f53d10981eebdeb4
bennk0509 <nkakiet21@clc.fitus.edu.vn>
2022-07-01 23:01:00 +0700

    login

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import user
 
 HOST = "127.0.0.1"
 SERVER_PORT = 65432
@@ -13,37 +14,21 @@ def sendList(client, list):
         client.recv(1024)
 
     msg = "end"
-    client.send(msg.encode(FORMAT))
-def checkValid(username, password):
-    if(len(username) > 32 and len(password) < 4):
-        return False
-    i= 0
-    p = "!@#$%^&*()-+"
-    while(i < len(password)):
-        if(password[i] >= 65 and password <= 90):
-            check1 = True
-        if(password[i] >= 97 and password <= 122):
-            check2 = True
-        if(password[i] >= 48 and password <= 57):
-            check3 = True
-        if(p.find(password[i]) != -1):
-            check4 = True
-        i+=1
-    return check1 and check2 and check3 and check4    
+    client.send(msg.encode(FORMAT))   
 
 def Login(client):
     account = []
     print("Please input username and password")
     username = input('Username:')
     password = input('Password:')
-    bool = checkValid(username, password)
-    while(bool == False):
+    bool = user.User.check_username_availability(username) and user.User.check_password(password)
+    while (bool == False):
        print('Please try again')
        print('The maximum length of username is 32 letters ')
        print('The min of pass is 4 and must have lowercase, uppercase, number, special character')
        username = input('Username:')
        password = input('Password:')
-       bool = checkValid(username, password)
+       bool = user.User.check_username_availability(username) and user.User.check_password(password)
     account.append(username)
     account.append(password)
     sendList(client, account)   
